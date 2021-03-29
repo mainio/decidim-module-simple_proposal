5886716b4b341fd05cc4fcc28bd4b6b842c23b9a
Eero Lahdenperä <eero.lahdenpera@mainiotech.fi>
2021-03-29 16:50:00 +0300

    Update tests

--- spec/system/create_proposal_spec.rb
+++ spec/system/create_proposal_spec.rb
@@ -11,6 +11,7 @@ describe "User creates proposal simply", type: :system do
   let!(:component) do
     create(:proposal_component,
            :with_creation_enabled,
+           :with_attachments_allowed_and_collaborative_drafts_enabled,
            manifest: manifest,
            participatory_space: participatory_process)
   end
@@ -25,52 +26,55 @@ describe "User creates proposal simply", type: :system do
     visit_component
   end
 
+  def fill_category_and_scope
+    select category.name["en"], from: :proposal_category_id
+    click_link "Global scope"
+    click_link scope.name["en"]
+    click_link "Select"
+  end
+
   context "when category and scope are required" do
     before do
       allow(Decidim::SimpleProposal).to receive(:require_category).and_return(true)
       allow(Decidim::SimpleProposal).to receive(:require_scope).and_return(true)
     end
 
-    it "doesnt create a new proposal without category and scope" do
-      click_link "New proposal"
-      fill_in :proposal_title, with: proposal_title
-      fill_in :proposal_body, with: proposal_body
-      select category.name["en"], from: :proposal_category_id
-      click_button "Continue"
-      expect(page).to have_css(".form-error")
-      expect(page).to have_content("can't be blank")
-    end
+    describe "proposal creation process" do
+      it "doesnt create a new proposal without category and scope" do
+        click_link "New proposal"
+        fill_in :proposal_title, with: proposal_title
+        fill_in :proposal_body, with: proposal_body
+        select category.name["en"], from: :proposal_category_id
+        click_button "Preview"
+        expect(page).to have_css(".form-error")
+        expect(page).to have_content("can't be blank")
+      end
 
-    it "creates a new proposal with a category and scope" do
-      click_link "New proposal"
-      fill_in :proposal_title, with: proposal_title
-      fill_in :proposal_body, with: proposal_body
-      select category.name["en"], from: :proposal_category_id
-      click_link "Global scope"
-      click_link scope.name["en"]
-      click_link "Select"
-      click_button "Continue"
-      click_button "Publish"
-      expect(page).to have_content("Proposal successfully published.")
-      expect(Decidim::Proposals::Proposal.last.category).to eq(category)
-      expect(Decidim::Proposals::Proposal.last.scope).to eq(scope)
-    end
+      it "creates a new proposal with a category and scope" do
+        click_link "New proposal"
+        fill_in :proposal_title, with: proposal_title
+        fill_in :proposal_body, with: proposal_body
+        fill_category_and_scope
+        click_button "Preview"
+        click_button "Publish"
+        expect(page).to have_content("Proposal successfully published.")
+        expect(Decidim::Proposals::Proposal.last.category).to eq(category)
+        expect(Decidim::Proposals::Proposal.last.scope).to eq(scope)
+      end
 
-    it "can be edited after creating a draft" do
-      click_link "New proposal"
-      fill_in :proposal_title, with: proposal_title
-      fill_in :proposal_body, with: proposal_body
-      select category.name["en"], from: :proposal_category_id
-      click_link "Global scope"
-      click_link scope.name["en"]
-      click_link "Select"
-      click_button "Continue"
-      click_link "Modify the proposal"
-      fill_in :proposal_title, with: "This proposal is modified"
-      click_button "Preview"
-      expect(page).to have_content("This proposal is modified")
-      click_button "Publish"
-      expect(page).to have_content("Proposal successfully published.")
+      it "can be edited after creating a draft" do
+        click_link "New proposal"
+        fill_in :proposal_title, with: proposal_title
+        fill_in :proposal_body, with: proposal_body
+        fill_category_and_scope
+        click_button "Preview"
+        click_link "Modify the proposal"
+        fill_in :proposal_title, with: "This proposal is modified"
+        click_button "Preview"
+        expect(page).to have_content("This proposal is modified")
+        click_button "Publish"
+        expect(page).to have_content("Proposal successfully published.")
+      end
     end
 
     context "when draft proposal exists for current users" do
@@ -103,9 +107,10 @@ describe "User creates proposal simply", type: :system do
       fill_in :proposal_title, with: proposal_title
       fill_in :proposal_body, with: proposal_body
       select category.name["en"], from: :proposal_category_id
-      click_button "Continue"
+      click_button "Preview"
       click_button "Publish"
       expect(page).to have_content("Proposal successfully published.")
     end
   end
+
 end
