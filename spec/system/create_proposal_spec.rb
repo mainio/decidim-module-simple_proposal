89ffe10b979b2864b3a936ab72f499c9d001d8d6
Eero Lahdenperä <eero.lahdenpera@mainiotech.fi>
2021-05-27 12:06:27 +0300

    Copy and modify create proposal test from master

--- spec/system/create_proposal_spec.rb
+++ spec/system/create_proposal_spec.rb
@@ -3,23 +3,31 @@
 require "spec_helper"
 
 describe "User creates proposal simply", type: :system do
-  include_context "with a component"
-  let(:organization) { create :organization, available_locales: [:en] }
+  let(:organization) { create :organization, *organization_traits, available_locales: [:en] }
   let(:participatory_process) { create :participatory_process, :with_steps, organization: organization }
   let(:manifest_name) { "proposals" }
+  let(:manifest) { Decidim.find_component_manifest(manifest_name) }
   let!(:user) { create :user, :confirmed, organization: organization }
   let!(:component) do
     create(:proposal_component,
            :with_creation_enabled,
-           :with_attachments_allowed_and_collaborative_drafts_enabled,
+           :with_attachments_allowed,
            manifest: manifest,
            participatory_space: participatory_process)
   end
-  let!(:scope) { create :scope, organization: organization }
-  let!(:category) { create :category, participatory_space: participatory_process }
+  let(:organization_traits) { [] }
 
   let(:proposal_title) { ::Faker::Lorem.paragraph }
   let(:proposal_body) { ::Faker::Lorem.paragraph }
+
+  def visit_component
+    if organization_traits.include?(:secure_context)
+      switch_to_secure_context_host
+    else
+      switch_to_host(organization.host)
+    end
+    page.visit main_component_path(component)
+  end
 
   before do
     login_as user, scope: :user
@@ -31,29 +39,6 @@ describe "User creates proposal simply", type: :system do
       allow(Decidim::SimpleProposal).to receive(:require_category).and_return(true)
       allow(Decidim::SimpleProposal).to receive(:require_scope).and_return(true)
     end
-
-    describe "proposal creation process" do
-      it "doesnt create a new proposal without category and scope" do
-        click_link "New proposal"
-        fill_in :proposal_title, with: proposal_title
-        fill_in :proposal_body, with: proposal_body
-        select category.name["en"], from: :proposal_category_id
-        click_button "Preview"
-        expect(page).to have_css(".form-error")
-        expect(page).to have_content("can't be blank")
-      end
-
-      it "creates a new proposal with a category and scope" do
-        click_link "New proposal"
-        fill_in :proposal_title, with: proposal_title
-        fill_in :proposal_body, with: proposal_body
-        fill_category_and_scope
-        click_button "Preview"
-        click_button "Publish"
-        expect(page).to have_content("Proposal successfully published.")
-        expect(Decidim::Proposals::Proposal.last.category).to eq(category)
-        expect(Decidim::Proposals::Proposal.last.scope).to eq(scope)
-      end
 
     context "when there is a scope and category" do
       let!(:scope) { create :scope, organization: organization }
@@ -70,8 +55,7 @@ describe "User creates proposal simply", type: :system do
           expect(page).to have_content("There's an error in this field")
         end
 
-      context "when uploading a file", processing_uploads_for: Decidim::AttachmentUploader do
-        it "can add image" do
+        it "creates a new proposal with a category and scope" do
           click_link "New proposal"
           fill_in :proposal_title, with: proposal_title
           fill_in :proposal_body, with: proposal_body
@@ -79,9 +63,9 @@ describe "User creates proposal simply", type: :system do
           click_button "Preview"
           click_button "Publish"
           expect(page).to have_content("Proposal successfully published.")
+          expect(Decidim::Proposals::Proposal.last.category).to eq(category)
+          expect(Decidim::Proposals::Proposal.last.scope).to eq(scope)
         end
-      end
-    end
 
         it "can be edited after creating a draft" do
           click_link "New proposal"
@@ -123,11 +107,18 @@ describe "User creates proposal simply", type: :system do
           scroll_to find(".button--nomargin.large")
         end
 
-      context "when uploading a file", processing_uploads_for: Decidim::AttachmentUploader do
-        it "shows error message when image is malicious" do
-          attach_file(:proposal_add_photos, Decidim::Dev.asset("malicious.jpg"))
+        it "can finish proposal" do
           click_button "Preview"
-          expect(page).to have_content("There was a problem saving the proposal")
+          click_button "Publish"
+          expect(page).to have_content("Proposal successfully published.")
+        end
+
+        context "when uploading a file", processing_uploads_for: Decidim::AttachmentUploader do
+          it "shows error message when image is malicious" do
+            attach_file(:proposal_add_photos, Decidim::Dev.asset("malicious.jpg"))
+            click_button "Preview"
+            expect(page).to have_content("There was a problem saving the proposal")
+          end
         end
       end
     end
