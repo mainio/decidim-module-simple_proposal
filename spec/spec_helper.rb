13a4a17429a24460c6e13853c4e521789c5d77f6
Joonas <joonas.aapro@mainiotech.fi>
2024-05-03 10:52:37 +0300

    Delete extra row from spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,4 +8,3 @@ Decidim::Dev.dummy_app_path =
   File.expand_path(File.join(__dir__, "decidim_dummy_app"))
 
 require "decidim/dev/test/base_spec_helper"
-require "#{Dir.pwd}/lib/decidim/simple_proposal/test/rspec_support/capybara.rb"
