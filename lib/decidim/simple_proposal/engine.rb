f213ba4f3abfe07c92cadf15b4d21f1a70fcaf86
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2025-08-28 16:00:55 +0300

    Fix incompatibility issue with the privacy module

--- lib/decidim/simple_proposal/engine.rb
+++ lib/decidim/simple_proposal/engine.rb
@@ -5,6 +5,29 @@ module Decidim
     class Engine < ::Rails::Engine
       isolate_namespace Decidim::SimpleProposal
 
+      if Decidim.module_installed?(:privacy)
+        # Fix incompatibility with the privacy module. The privacy module sets
+        # itself as the first priority when loading view paths which would break
+        # the functionality in this module overriding some of the same views in
+        # proposals.
+        initializer "decidim_privacy.prepend_view_path", after: "decidim_privacy.prepend_view_path" do
+          config.after_initialize do
+            # Append the engine view path **BEFORE** the privacy module's view
+            # path in order to take priority over that module.
+            paths = []
+            ActionController::Base.view_paths.paths.each do |resolver|
+              if resolver.path.starts_with?("#{Decidim::Privacy::Engine.root}/app/views")
+                # Inject before privacy.
+                paths << "#{Decidim::SimpleProposal::Engine.root}/app/views"
+              end
+
+              paths << resolver
+            end
+            ActionController::Base.view_paths = paths
+          end
+        end
+      end
+
       initializer "decidim_proposals.overrides" do |app|
         app.config.to_prepare do
           Decidim::Proposals::ProposalsController.include Decidim::SimpleProposal::ProposalsControllerOverride
