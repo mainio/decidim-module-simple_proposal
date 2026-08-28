aad2e1753edb2c4651c7147a3d62726d36b8ead5
Joonas <joonas.aapro@mainiotech.fi>
2026-08-28 10:13:39 +0300

    Remove old and deprecated migration

--- db/migrate/20220318112320_add_deleted_at_to_proposals.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AddDeletedAtToProposals < ActiveRecord::Migration[6.0]
-  def change
-    add_column :decidim_proposals_proposals, :deleted_at, :datetime
-    add_index :decidim_proposals_proposals, :deleted_at
-  end
-end
